9fd80e544c195625d60253eeb671c927707c6b0d
Cloyce Stetson <crs170030@utdallas.edu>
2022-04-08 18:03:27 -0500

    The Way I Use My Walk
    Added a walk animation for Kazimir in the overworld. Next is environment design? Maybe enemies?

--- Assets/_Game/Scripts/PlayerMovement.cs
+++ Assets/_Game/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Camera mainCamera = null;
 
+    [SerializeField] Animator animator = null;
+
     //Rigidbody _rb = null;
     public CharacterController controller;
 
@@ -47,6 +49,28 @@ public class PlayerMovement : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
+        animator.SetInteger("xInput", (int)Mathf.Ceil(h));
+        animator.SetInteger("yInput", (int)Mathf.Ceil(v));
+        if(h == 0 && v == 0)
+        {
+            if (animator.speed > 0)
+            {
+                animator.speed -= 0.05f;
+            }
+            else
+            {
+                animator.speed = 0;
+                //animator.time = 0;
+                jumpToTime(currentAnimationName(), 0f);
+            }
+        }
+        else
+        {
+            animator.speed = 1;
+        }
+        //animator.SetFloat("xInput", h);
+        //animator.SetFloat("yInput", v);
+
         //Running
         if (Input.GetKey(KeyCode.LeftShift) && isGrounded)
         {
@@ -58,8 +82,8 @@ public class PlayerMovement : MonoBehaviour
             runMultiplyer = 1f;
         }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        //float h = Input.GetAxis("Horizontal");
+        //float z = Input.GetAxis("Vertical");
 
         //move character with a and d
         Vector3 move = ((transform.right * h) + (transform.forward * v));
@@ -79,5 +103,24 @@ public class PlayerMovement : MonoBehaviour
 
         //move with velocity with time squared
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    void jumpToTime(string name, float nTime)
+    {
+        animator.Play(name, 0, nTime);
+    }
+
+    string currentAnimationName()
+    {
+        var currAnimName = "";
+        foreach (AnimationClip clip in animator.runtimeAnimatorController.animationClips)
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName(clip.name))
+            {
+                currAnimName = clip.name.ToString();
+            }
+        }
+
+        return currAnimName;
     }
 }
