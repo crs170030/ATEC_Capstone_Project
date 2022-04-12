726aa64dd9202d182fdb7706299d9e45dff4f6af
Cloyce Stetson <crs170030@utdallas.edu>
2022-04-12 14:26:53 -0500

    Enemy Movement 1
    Backup before nav mesh

--- Assets/_Game/Scripts/EnemyMovement.cs
+++ Assets/_Game/Scripts/EnemyMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
     void Start()
     {
-        
+
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
